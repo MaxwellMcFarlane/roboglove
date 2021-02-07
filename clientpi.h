dc465c0c1e4e1d9b313c1ab05b4283b2ded959b5
Maxwell McFarlane <32963706+MaxwellMcFarlane@users.noreply.github.com>
2021-02-07 13:49:23 -0500

    Add files via upload

---
+++ clientpi.h
@@ -0,0 +1,23 @@
+#ifndef CLIENTPI_H
+#define CLIENTPI_H
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define PORTNO 51717
+
+static char buffer[256];
+static int sockfd,newsockfd,clilen;
+static struct sockaddr_in serv_addr,cli_addr;
+static int n;
+
+void error(char *msg);
+//This function connects the client to the serverpi
+int socketClientConnect();
+//This function returns the buffer used in file descriptor of the socket
+uint16_t * getBuffer();
+
+#endif // CLIENTPI_H
